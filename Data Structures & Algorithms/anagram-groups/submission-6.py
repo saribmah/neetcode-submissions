8a222a396b2cff233cf4da840bbf7fb4d0cf8523
Sarib Mahmood <96064700+saribmah@users.noreply.github.com>
2026-05-30 10:49:09 -0400

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hmap = {}
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            v = ",".join(str(x) for x in count) 
+            if v not in hmap:
+                hmap[v] = []
+            hmap[v].append(s)
+
+        return list(hmap.values())
