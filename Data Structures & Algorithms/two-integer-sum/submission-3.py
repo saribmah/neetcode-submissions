5a4513740202525f211a349e2a32fe48229d77f8
Sarib Mahmood <96064700+saribmah@users.noreply.github.com>
2026-06-02 13:02:55 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap = {}
+
+        for index, num in enumerate(nums):
+            remainder = target - num
+            if remainder in hmap:
+                return [hmap[remainder], index]
+            hmap[num] = index
+
+        return []
