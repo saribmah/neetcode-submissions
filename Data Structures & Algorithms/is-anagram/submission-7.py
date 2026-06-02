fe44c0b9a1ae79b004a2edc0469f248fe2fffd3e
Sarib Mahmood <96064700+saribmah@users.noreply.github.com>
2026-06-02 13:00:31 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count1 = [0] * 26
+        count2 = [0] * 26
+
+        for c in s:
+            count1[ord(c) - ord('a')] += 1
+
+        for c in t:
+            count2[ord(c) - ord('a')] += 1
+
+
+        return tuple(count1) == tuple(count2)
+        
